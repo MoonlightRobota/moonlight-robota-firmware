d3dd56ab9723b1317f87d2bdf6a7e0070cc3346a
Enkhbold470 <enkhbold470@gmail.com>
2024-04-21 05:47:20 -0700

    fixed

--- moon-light-robot-firmware.ino
+++ moon-light-robot-firmware.ino
@@ -1,66 +1,112 @@
 #define PWMB 9
 #define BIN2 8
 #define BIN1 7
-#define AIN1 4
-#define AIN2 5
-#define PWMA 6
+#define STBY 6
+#define AIN1 5
+#define AIN2 4
+#define PWMA 3
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Serial started...");
 
   pinMode(PWMB, OUTPUT);
   pinMode(BIN2, OUTPUT);
   pinMode(BIN1, OUTPUT);
+  pinMode(STBY, OUTPUT);
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(PWMA, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
   pinMode(A0, OUTPUT);
   buzzer(1);
-  pinMode(12, INPUT_PULLUP);
   do {
+    digitalWrite(STBY, 1);
+
     car();
 
-  } while (digitalRead(12) != 0);
+  } while (digitalRead(2) != LOW);
+  digitalWrite(STBY, 1);
+
   Serial.println("Started...");
   buzzer(2);
-//   motor(255, 255);
-//   delay(2000);
-//   motor(0, 0);
+  //   motor(255, 255);
+  //   delay(2000);
+  //   motor(0, 0);
+
+
+
+
+  motor(-255, 255);
+  delay(1000);
+  motor(0, 0);
+  delay(2000);
+  motor(255, -255);
+  delay(1000);
+
+  motor(0, 0);
+
+  // delay(2500);
+  // motor(-155, -155);
+  // motor(-255, -255);
+  // delay(1500);
+  // motor(0, 0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
+void checkFront() {
+  delay(1000);
+  motor(255, 255);
+  delay(2000);
+  motor(0, 0);
+  delay(5000);
+  motor(-255, -255);
+  delay(2000);
+  motor(0, 0);
+  delay(2000);
+}
+void checkRight() {
+  delay(500);
+  motor(-255, 255);
+  delay(500);
+  motor(0, 0);
+  delay(2000);
+  motor(255, -255);
+  delay(600);
+  motor(0, 0);
+  delay(2000);
+}
 void car() {
   if (Serial.available() > 0) {
-    
     char move = Serial.read();
-    Serial.print("serial received: ");
-    Serial.println(move);
-    
+    // Serial.println(move);
     motor(0, 0);
-    analogWrite(10, 0);
-    int speed = 255;
     switch (move) {
-
       case 'F':
-        motor(speed, speed);
-        break;
-      case 'B':
-        motor(-speed, -speed);
+        delay(500);
+        motor(255, 255);
+        delay(1000);
+        motor(0, 0);
+        delay(2500);
+
         break;
       case 'R':
-        motor(speed, 0);
+        delay(500);
+        motor(-255, 255);
+        delay(500);
+        motor(0, 0);
+        delay(2500);
+    
         break;
       case 'L':
-        motor(0, speed);
-        break;
-      case 'V':
-        buzzer(2);
-        break;
-      case 'X':
-        speed = 0;
+        delay(500);
+        motor(255, -255);
+        delay(500);
+        motor(0, 0);
+        delay(2500);
+     
         break;
     }
   }
@@ -84,10 +130,6 @@ void motor(int pwma, int pwmb) {
   } else if (pwma > 0) {
     digitalWrite(AIN1, 1);
     digitalWrite(AIN2, 0);
-
-  } else {
-    digitalWrite(AIN1, 1);
-    digitalWrite(AIN2, 1);
   }
   if (pwmb > 0) {
     digitalWrite(BIN1, 0);
@@ -95,9 +137,5 @@ void motor(int pwma, int pwmb) {
   } else if (pwmb < 0) {
     digitalWrite(BIN1, 1);
     digitalWrite(BIN2, 0);
-
-  } else {
-    digitalWrite(BIN1, 1);
-    digitalWrite(BIN2, 1);
   }
 }
